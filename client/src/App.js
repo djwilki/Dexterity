1b342d73213f060bd6f95b669eaae85edddd85f1
Daniel Wilkins <11danielw@gmail.com>
2020-12-18 13:51:36 -0800

    Implement first material-ui component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
-
 import UserList from './components/UsersList';
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
 
 function App() {
   console.log("____Rendering app_____")
+
+  const classes = useStyles();
   return (
     <BrowserRouter>
+    <AppBar position="sticky">
+  <Toolbar>
+    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+      <MenuIcon />
+    </IconButton>
+    <Typography variant="h6" className={classes.title}>
+      News
+    </Typography>
+    <Button color="inherit">Login</Button>
+  </Toolbar>
+</AppBar>
         <nav>
             <ul>
                 <li><NavLink to="/" activeclass="active">Home</NavLink></li>
