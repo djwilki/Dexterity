12d7a8a1d2f29fc8ee08d5bd65297044ffec5c8f
Daniel Wilkins <11danielw@gmail.com>
2020-12-22 16:03:01 -0800

    Add custom menu react component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, NavLink, Link as RouterLink } from 'react-router-dom';
 import UserList from './components/UsersList';
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
@@ -8,37 +8,75 @@ import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import DropDownMenu from './components/DropDownMenu'
+
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }));
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  navbar: {
+    backgroundColor: 'inherit',
+    boxShadow: 'none',
+    display: 'flex',
+    justifyContent: 'center'
+  }
+}));
+
+
+
+
 
 function App() {
   console.log("____Rendering app_____")
 
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl2, setAnchorEl2] = React.useState(null);
+
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleClick2 = (event) => {
+    setAnchorEl2(event.currentTarget);
+  };
+
+  const handleClose2 = () => {
+    setAnchorEl2(null);
+  };
   const classes = useStyles();
   return (
     <BrowserRouter>
-    <AppBar position="sticky">
-  <Toolbar>
-    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-      <MenuIcon />
-    </IconButton>
-    <Typography variant="h6" className={classes.title}>
-      News
-    </Typography>
-    <Button color="inherit">Login</Button>
-  </Toolbar>
-</AppBar>
-        <nav>
+      <AppBar className={classes.navbar} position="sticky">
+        <Toolbar className={classes.navbar}>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Button className={classes.menuButton} component={RouterLink} to='/'>HOME</Button>
+          <DropDownMenu title="ABOUT" links={[['/staff', 'STAFF']]} ></DropDownMenu>
+          <DropDownMenu title="SERVICES" links={[['/physicaltherapy', 'PHYSICAL THERAPY'], ['/telehealth', 'TELEHEALTH']]} ></DropDownMenu>
+          <Button component={RouterLink} to='/education'>EDUCATION</Button>
+          <Button component={RouterLink} to='/info'>INFO</Button>
+          <Button component={RouterLink} to='/contact'>CONTACT</Button>
+        </Toolbar>
+
+
+      </AppBar>
+      {/* <nav>
             <ul>
                 <li><NavLink to="/" activeclass="active">Home</NavLink></li>
                 <li><NavLink to="/users" activeclass="active">Users</NavLink></li>
@@ -52,7 +90,7 @@ function App() {
             <Route path="/">
                 <h1>My Home Page</h1>
             </Route>
-        </Switch>
+        </Switch> */}
     </BrowserRouter>
   );
 }
---
+++ client/src/components/DropDownMenu.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route, NavLink, Link as RouterLink } from 'react-router-dom';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button'
+
+export default function DropDownMenu({links, title, }) {
+
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+      };
+
+      const handleClose = () => {
+        setAnchorEl(null);
+      };
+
+    const items = links.map(ele => {
+         return <MenuItem component={RouterLink} to={ele[0]} onClick={handleClose}>{ele[1]}</MenuItem>
+        })
+
+
+
+
+
+return (
+    <div>
+        <Button aria-controls="simple-menu" aria-haspopup="true" onMouseOver={handleClick}>
+        {title}
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        PaperProps={{
+            style: {
+              transform: 'translateX(0%) translateY(10px)',
+            }
+          }}
+        MenuListProps={{ onMouseLeave: handleClose }}
+      >
+        {title.length > 0 ? <>{items}</>: <></>}
+      </Menu>
+    </div>
+)
+
+}
