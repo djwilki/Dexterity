87b89a574ff147ab131eee36676b6594634db0b9
Daniel Wilkins <11danielw@gmail.com>
2020-12-28 14:44:55 -0800

    Add props to icon strip component

--- client/src/components/IconStrip.js
+++ client/src/components/IconStrip.js
@@ -3,7 +3,7 @@ import Box from '@material-ui/core/Box'
 import ShowChart from '@material-ui/icons/ShowChart'
 import Typography from '@material-ui/core/Typography'
 
-export default function IconStrip() {
+export default function IconStrip({title, imgSrc}) {
 
 
 
@@ -11,10 +11,10 @@ export default function IconStrip() {
         <Box style={{display: 'flex', flexDirection: 'row'}}>
             <Box style={{ color: 'white', backgroundColor: 'blue', height: '300px', width: '50vw', display:'flex', flexDirection: 'column',  justifyContent: 'center', alignItems: 'center' }}>
                 <ShowChart style={{fontSize: '200px'}}></ShowChart>
-                <Typography>{'HeartRate'}</Typography>
+                <Typography>{title}</Typography>
             </Box>
             <Box>
-                <img style={{height: '300px', width: '50vw'}} src={'https://images.unsplash.com/photo-1555656220-46e30749d330?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80'}></img>
+                <img style={{height: '300px', width: '50vw'}} src={imgSrc}></img>
             </Box>
         </Box>
     )
