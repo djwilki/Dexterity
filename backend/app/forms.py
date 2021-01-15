494f602e48726948a746e9083a9e7579ec0b574e
Daniel Wilkins <11danielw@gmail.com>
2021-01-14 22:22:58 -0800

    Add login form

--- backend/app/forms.py
+++ backend/app/forms.py
@@ -5,3 +5,6 @@ from wtforms.validators import InputRequired, Length, Email
 
 class EmailForm(FlaskForm):
     userEmail = StringField("Email", validators=[InputRequired("Please provide a valid email."), Email()])
+
+class LoginForm(FlaskForm):
+    emailOrUsername = StringField("EmailOrUsername", validators=[InputRequired("Please provide a valid username or email.")])
