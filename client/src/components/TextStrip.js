1b9ca9be1db728620e80eace89bd15d0e2f48b8b
Daniel Wilkins <11danielw@gmail.com>
2021-01-20 19:57:06 -0800

    Create text strip component

---
+++ client/src/components/TextStrip.js
@@ -0,0 +1,26 @@
+import { Box } from '@material-ui/core';
+import React from 'react';
+import Box from '@material-ui/core/Box'
+import { makeStyles } from '@material-ui/core/styles';
+
+
+export default function TextStrip(){
+
+    const useStyles = makeStyles((theme) => ({
+        TextStrip: {
+          justifyContent: 'center',
+        },
+        TextStripContact: {
+            fontWeight: 'bold'
+        }
+      }));
+
+      const classes = useStyles();
+
+return(
+<Box className={classes.TextStrip}>
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+</Box>
+)
+
+}
