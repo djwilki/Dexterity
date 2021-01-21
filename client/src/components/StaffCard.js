fe30a7616d7bbc0a2581afb644ace125064bd973
Daniel Wilkins <11danielw@gmail.com>
2021-01-20 20:32:00 -0800

    Add telehealth page content

--- client/src/components/StaffCard.js
+++ client/src/components/StaffCard.js
@@ -1,13 +1,13 @@
 import { Box } from '@material-ui/core';
 import React from 'react';
 
-export default function Component(name, position){
+export default function Component(name, position) {
 
-return(
-<Box>
-<img src='' style={{borderRadius: '50%'}}/>
-<h3>{name},{position}</h3>
-</Box>
-)
+    return (
+        <Box>
+            <img src='' style={{ borderRadius: '50%' }} />
+            <h3>{name},{position}</h3>
+        </Box>
+    )
 
 }
---
+++ client/src/pages/telehealth.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {loadStripe} from '@stripe/stripe-js';
+import { Box } from '@material-ui/core';
+import InlineCard from '../components/InlineCard'
+import TextStrip from '../components/TextStrip';
+
+
+export default function Telehealth(){
+
+
+    const stripePromise = loadStripe('pk_test_JJ1eMdKN0Hp4UFJ6kWXWO4ix00jtXzq5XG');
+
+
+return(
+    <Box>
+        <InlineCard></InlineCard>
+        <TextStrip></TextStrip>
+    </Box>
+)
+
+}
