f9514d73158218f44b6241b0683aa3e0a79512d2
Daniel Wilkins <11danielw@gmail.com>
2021-01-20 20:17:29 -0800

    Add staff card component

---
+++ client/src/components/StaffCard.js
@@ -0,0 +1,13 @@
+import { Box } from '@material-ui/core';
+import React from 'react';
+
+export default function Component(name, position){
+
+return(
+<Box>
+<img src='' style={{borderRadius: '50%'}}/>
+<h3>{name},{position}</h3>
+</Box>
+)
+
+}
