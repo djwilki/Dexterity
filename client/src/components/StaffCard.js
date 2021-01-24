ec2a8df59b15fcdc1cdaed129b01d65ea2197410
Daniel Wilkins <11danielw@gmail.com>
2021-01-23 21:52:57 -0800

    Add Staffcard image prop

--- client/src/components/StaffCard.js
+++ client/src/components/StaffCard.js
@@ -1,11 +1,11 @@
 import { Box } from '@material-ui/core';
 import React from 'react';
 
-export default function Component(name, position) {
+export default function Component(name, position, image) {
 
     return (
         <Box>
-            <img src='' style={{ borderRadius: '50%' }} />
+            <img src={image} style={{ borderRadius: '50%' }} />
             <h3>{name},{position}</h3>
         </Box>
     )
