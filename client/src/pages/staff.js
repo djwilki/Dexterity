297b49ff9914196437ec8f8b9f0b2ed85508f2d8
Daniel Wilkins <11danielw@gmail.com>
2021-01-25 21:18:19 -0800

    Staffcard styling

--- client/src/pages/staff.js
+++ client/src/pages/staff.js
@@ -10,7 +10,7 @@ export default function Newsletter(){
 return(
 <>
     <StaffCard name='Stacy' position='temp' path='../../public/images/pt.jpg'></StaffCard>
-    <Box style={{display: 'flex', width: '66vw'}}>
+    <Box style={{display: 'flex', width: '66vw', justifyContent: 'center'}}>
         <StaffCard name='Stacy' position='temp' path='../../public/images/pt.jpg'></StaffCard>
         <StaffCard name='Stacy' position='temp' path='../../public/images/pt.jpg'></StaffCard>
         <StaffCard name='Stacy' position='temp' path='../../public/images/pt.jpg'></StaffCard>
