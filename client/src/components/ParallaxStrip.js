6c881890fb0da35270c605e6de40486e4c9c7c58
Daniel Wilkins <11danielw@gmail.com>
2021-02-03 20:48:10 -0800

    Add parallaxstrip props

--- client/src/components/ParallaxStrip.js
+++ client/src/components/ParallaxStrip.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Box from '@material-ui/core/Box'
 import { makeStyles } from '@material-ui/core';
 
-export default function ParallaxStrip(){
+export default function ParallaxStrip({url}){
 
     const useStyles = makeStyles((theme)=> ({
         parallax: {
-            backgroundImage: 'url("https://images.unsplash.com/photo-1555656220-46e30749d330?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80")',
+            backgroundImage: `url(${url})`,
             height: "500px",
             backgroundAttachment: "fixed",
             backgroundPosition: "center",
