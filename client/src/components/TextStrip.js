30484830037e993aeb92a6676bd44d2b38487bb6
Daniel Wilkins <11danielw@gmail.com>
2021-02-03 20:43:48 -0800

    Generalize textstrip component

--- client/src/components/TextStrip.js
+++ client/src/components/TextStrip.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
 
-export default function TextStrip(){
+export default function TextStrip({text}){
 
     const useStyles = makeStyles((theme) => ({
         TextStrip: {
@@ -18,7 +18,7 @@ export default function TextStrip(){
 
 return(
 <Box className={classes.TextStrip}>
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  {text}
 </Box>
 )
 
