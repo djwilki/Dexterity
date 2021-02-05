07dee8375457fa6ecb2d4e9d06c14373867bdf1e
Daniel Wilkins <11danielw@gmail.com>
2021-02-04 20:48:21 -0800

    Carousel animation change

--- client/src/App.js
+++ client/src/App.js
@@ -140,7 +140,7 @@ function App() {
             <img className={classes.carouselImage} src='https://cdn.pixabay.com/photo/2017/07/25/16/49/manual-release-2538802_960_720.jpg'></img>
           </Carousel>
           <ParallaxStrip></ParallaxStrip>
-          <Carousel animation='slide' navButtonsAlwaysVisible={visible} >
+          <Carousel animation='fade' navButtonsAlwaysVisible={visible} >
             <img className={classes.carouselImage} src='https://cdn.pixabay.com/photo/2018/06/02/18/22/fitness-3448864_960_720.jpg'></img>
             <img className={classes.carouselImage} src='https://cdn.pixabay.com/photo/2020/05/30/18/05/trauma-5239844_960_720.jpg'></img>
             <img className={classes.carouselImage} src='https://cdn.pixabay.com/photo/2017/07/25/16/49/manual-release-2538802_960_720.jpg'></img>
